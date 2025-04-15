a03b962484559167df43fb5e0397e84b4bf1809f
awale-web <aden.hirmoge@gmai.com>
2025-04-15 22:06:32 +0300

    Update Gallery layout and styling

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,41 +2,27 @@ import React from 'react';
 import '../style/Gallery.css';
 
 const Gallery = () => {
+  const base = import.meta.env.BASE_URL;
+
+  const galleryItems = [
+    { src: 'eventdecor.jpg', label: 'Event Decoration' },
+    { src: 'birthday-fun.jpg', label: 'Birthday Celebration' },
+    { src: 'wedding-moment.jpg', label: 'Wedding Moment' },
+    { src: 'conference-hall.jpg', label: 'Conference Setup' },
+    { src: 'evening-party.jpg', label: 'Evening Party' },
+    { src: 'baby-shower.jpg', label: 'Baby shower' },
+  ];
+
   return (
     <section className="gallery">
       <h2>Our Event Gallery</h2>
       <div className="gallery-container">
-
-        <div className="gallery-item">
-          <img src="/images/baby-showering.jpg" alt="Baby Shower" />
-          <p>Baby Shower</p>
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/baby-shower.jpg" alt="Baby Shower" />
-          <p>Baby Shower</p>
-        </div>
-        
-        <div className="gallery-item">
-          <img src="/images/Somali-wedding.jpg" alt="Wedding" />
-          <p>Somali Wedding</p>
-        </div>
-        
-        <div className="gallery-item">
-          <img src="/images/wedding.jpg" alt="Wedding" />
-          <p>Wedding</p>
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/Anniversary- party.jpg" alt="Anniversary" />
-          <p>Anniversary</p>
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/birthday-party.jpg" alt="Birthday" />
-          <p>Birthday</p>
-        </div>
-
+        {galleryItems.map((item, index) => (
+          <div className="gallery-item" key={index}>
+            <img src={`${base}images/${item.src}`} alt={item.label} />
+            <p>{item.label}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
